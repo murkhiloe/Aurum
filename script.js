65c5760923c72f147a4071f9dc4fec929c7c6edf
murkhiloe <murkhiloe@gmail.com>
2025-07-02 08:03:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sample product data
+const products = {
+  kicks: [/* ... kicks products ... */],
+  iphones: [/* ... iPhones products ... */],
+  caps: [/* ... caps products ... */]
+};
+// ... remaining JS functions ...
